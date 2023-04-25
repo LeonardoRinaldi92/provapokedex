47b86be886d7d678bdd11aed5a158e79ef0d09fc
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-04-25 16:56:00 +0200

    struttura dati pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,45 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        listapokemon : [],
-        numero : 1,
-        visibilitaSopra : true,
-        numerosopra : this.numeroSopra()
+        listapokemon :[],
       }
     },
     methods: {
-        trovaMail () {
-            axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then( (res) => 
-            {this.listapokemon = res.data.results
-            console.log(this.listapokemon)})
-        },
-        numeropiu() {
-            this.numero ++
-        },
-        numeroSopra() {
-            let  numero = 1
-            if (this.numero - 1 ==! 1 || this.numero == 1) {
-                visibilitaSopra = true 
-                return this.numero - 1
-            } else {
-                visibilitaSopra = false
-                return this.numero - 1
+        trovapokemon () {
+            for (let i = 1; i <=151; i++){
+                let pokemon ={};
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then( (res) => 
+                {pokemon.name = res.data.forms[0].name;
+                pokemon.id = res.data.id;
+                pokemon.image = res.data.sprites.other.dream_world.front_default;
+                this.listapokemon.push(pokemon)
+                })
             }
-
-        }
+            console.log(this.listapokemon)
+        },
     },
     created() {
-        this.trovaMail()
+        this.trovapokemon()
     },
+
+        // numeropiu() {
+        //     this.numero ++
+        // },
+        // numeroSopra() {
+        //     let  numero = 1
+        //     if (this.numero - 1 ==! 1 || this.numero == 1) {
+        //         visibilitaSopra = true 
+        //         return this.numero - 1
+        //     } else {
+        //         visibilitaSopra = false
+        //         return this.numero - 1
+        //     }
+
+    //     }
+    // },
+    // created() {
+    //     this.trovaMail()
+    // },
 
 
 }).mount('#app')
